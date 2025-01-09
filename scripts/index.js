80e74e14f0dd07066315c57391a9ed517c8e3e86
davidmatei1902 <davidmatei1902@gmail.com>
2025-01-09 20:34:21 +0200

    Search bar ready. Only static components of search bar appears.

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,44 @@
 document.addEventListener("DOMContentLoaded", (_event) => {
     alert("After DOM has loaded");
     // todo: Add code here that updates the HTML, registers event listeners, calls HTTP endpoints, etc.
+
+    let availableKeywords = [
+        'phrase 1',
+        'how to use a shovel',
+        'what year is now?',
+        'what month is special?',
+        'how to send stuff to friends',
+        'loremipsum',
+        'love',
+    ];
+
+    const resultBox = document.querySelector(".result-box");
+    const inputBox = document.getElementById("input-box");
+
+    inputBox.onkeyup = function() {
+        let result = [];
+        let input = inputBox.value;
+        if(input.length)
+        {
+            result = availableKeywords.filter((keyword) => {
+               return keyword.toLowerCase().includes(input.toLowerCase());
+            });
+            console.log(result);
+        }
+        display(result);
+    }
+
+    function display(result) {
+        const content = result.map((list) => {
+            return "<li>" + list + "</li>" 
+        });
+
+        resultBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+    }
+
+    resultBox.addEventListener("click", (event) => {
+        if (event.target.tagName === "LI") {
+            inputBox.value = event.target.innerHTML;
+        }
+    });
 });
